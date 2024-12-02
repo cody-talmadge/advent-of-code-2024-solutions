605fa20ec11945f4c8c83cce746a5bd7d54c8584
Cody-Talmadge <Cody.Talmadge@gmail.com>
2024-12-02 09:46:07 -0500

    Refactored for readability

--- day-2/part-1.py
+++ day-2/part-1.py
@@ -1,32 +1,23 @@
 safe_count = 0
 
-input = []
+def check_numbers(numbers):
+    incrementing = numbers[1] > numbers[0]
+
+    min_diff = 1 if incrementing else -3
+    max_diff = 3 if incrementing else -1
+
+    for i in range(len(numbers) - 1):
+        diff = numbers[i+1] - numbers[i]
+        if diff > max_diff or diff < min_diff:
+            return False
+
+    return True
+
 with open('input.txt') as file:
     for line in file:
         numbers = line.split(' ')
         numbers = [int(number) for number in numbers]
-        if numbers[1] > numbers[0]:
-            incrementing = True
-        else:
-            incrementing = False
-
-        safe = 1
-        if incrementing:
-            for i in range(len(numbers) - 1):
-                diff = numbers[i + 1] - numbers[i]
-                print(numbers, diff)
-                if diff > 3:
-                    safe = 0
-                if diff < 1:
-                    safe = 0
-        else:
-            for i in range(len(numbers) - 1):
-                diff = numbers[i] - numbers[i + 1]
-                if diff > 3:
-                    safe = 0
-                if diff < 1:
-                    safe = 0
-        print(safe)
-        safe_count += safe
+        if check_numbers(numbers):
+            safe_count += 1
 
 print(safe_count)
